ef7ef8e6e8d159d27e9da98a1f1a6e8794008ee1
xdinh99z <xdinh99z@gmai.com>
2022-07-11 17:00:40 +0700

    Update code 2

--- src/main/java/com/xdinh99z/calculator/CalculatorApplication.java
+++ src/main/java/com/xdinh99z/calculator/CalculatorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApplication {
-
+	private static final String constant = "constant";
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
