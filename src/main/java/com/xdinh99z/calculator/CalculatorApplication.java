089eb904c7b4cb11de3ead0c92d643f7e46c426f
xdinh99z <51411628+xdinh99z@users.noreply.github.com>
2022-08-25 15:22:13 +0700

    Update code hazelcast

--- src/main/java/com/xdinh99z/calculator/CalculatorApplication.java
+++ src/main/java/com/xdinh99z/calculator/CalculatorApplication.java
@@ -1,12 +1,20 @@
 package com.xdinh99z.calculator;
 
+import com.hazelcast.client.config.ClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCaching
 public class CalculatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
+	}
+@Bean
+public ClientConfig hazelcastClientConfig() {
+	ClientConfig clientConfig = new ClientConfig();
+	clientConfig.getNetworkConfig().addAddress("hazelcast");
+	return clientConfig;
 	}
 
 }
